5b9d54f970f9d04a2fa8c7be2daf5f464deb2f0f
Michael-49 <84306089+Michael-49@users.noreply.github.com>
2022-11-02 09:30:56 +0530

    Add files via upload

---
+++ Q6/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieBookingApp
+{
+    public class Movie
+    {
+        public int  Id { get; set; }
+        public string Title { get; set; }
+        public string Cast { get; set; }
+        public double TicketPrice { get; set; }
+        public Movie( int id,string title, string cast, double price)
+        {
+            Id = id;
+            Title = title;
+            Cast = cast;
+            TicketPrice = price;
+        }
+    }
+}
---
+++ Q6/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieBookingApp
+{
+    public class Ticket
+    {
+        public Movie Movie { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int TicketCount { get; set; }
+        public double TotalAmount { get; set; }
+
+        public Ticket(Movie movie, DateTime date, int ticketCount, double totalAmount)
+        {
+            Movie = movie;
+            BookingDate = date;
+            TicketCount = ticketCount;
+            TotalAmount = totalAmount;
+        }
+    }
+}
